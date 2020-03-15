a5ca3e845779c4eb9eaa6a18f5cd948a19f83ab3
KaimNotark <KaimNotark@yandex.ru>
2020-03-15 23:59:56 +0800

    fix bag in jm1 with searchin zero in string

--- src/components/jm1.vue
+++ src/components/jm1.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="container">
-    <h2 class="subtitle">Задача 1</h2>
+    <h2 class="subtitle _green">Задача 1</h2>
     <p class="text">
-      Функция first() принимает строку, содержащую буквы и цифры
-      В строке нужно удалить все повторяющиеся подряд цифры
-      Повторяющиеся буквы оставляем без изменений
-      <br />first("abcd111"); // "abcd1"
-      <br />first("aaaa22122bbbb"); // "aaaa212bbbb"
-      <br />first("a8n64m33xxx000x0"); // "a8n64m3xxx0x0"
+      Функция first() принимает строку, содержащую буквы и цифры.
+      В строке нужно удалить все повторяющиеся подряд цифры.
+      Повторяющиеся буквы оставляем без изменений.
     </p>
-    <h3 class="sub-subtitle">Решение:</h3>
+    <p class="text">
+      <span class="_green">Пример:</span>
+    </p>
+    <ul>
+      <li>first("abcd111"); // "abcd1"</li>
+      <li>first("aaaa22122bbbb"); // "aaaa212bbbb"</li>
+      <li>first("a8n64m33xxx000x0"); // "a8n64m3xxx0x0"</li>
+    </ul>
+    <h3 class="sub-subtitle _green">Решение:</h3>
     <p class="text">Введите строку:</p>
     <input type="text" size="50" class="users-string" v-model="originalString" />
-    <p class="string">
+    <p class="text">
       <span class="_green">Вы ввели:</span>
       " {{ originalString }} "
     </p>
@@ -21,10 +26,11 @@
       type="button"
       class="button"
       @click="onAnalysis"
-    >Выдать решение</b-button>
-    <p class="string">
+    >Показать результат</b-button>
+    <p class="text">
       <span class="_green">Результат:</span>
-      " {{ finalString }} "
+      "
+      <span class="_red">{{ finalString }}</span> "
     </p>
     <hr />
   </div>
@@ -49,14 +55,16 @@ export default {
       // console.log("JM1--onAnalysis - lengthOfString= " + this.lengthOfString);
 
       for (let i = 0; i < this.lengthOfString; i++) {
-        if (parseInt(this.originalString[i])) {
-          if (!parseInt(this.originalString[i + 1])) {
+        if ( parseInt(this.originalString[i], 10)>=0 ) {
+          // console.log(i + "- " + parseInt(this.originalString[i], 10));
+
+          if ( !(parseInt(this.originalString[i + 1], 10)>=0 )) {
             this.finalString += this.originalString[i];
           } else {
             if (
               !(
-                parseInt(this.originalString[i]) ==
-                parseInt(this.originalString[i + 1])
+                parseInt(this.originalString[i], 10) ==
+                parseInt(this.originalString[i + 1], 10)
               )
             )
               this.finalString += this.originalString[i];
@@ -73,7 +81,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../stylesheets/variables.scss";
+@import "../stylesheets/resets.scss";
+
+%text-input {
+  color: $color-second;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 12px;
+}
+
+%input {
+  height: 45px;
+  border: 1px solid $color-second;
+  background-color: $color-white;
+  border-radius: 6px;
+  padding: 10px;
+}
+
+.users-string {
+  @extend %text-input;
+  @extend %input;
+}
+.text {
+  width: 700px;
+  margin-bottom: 5px;
+}
+
+.sub-subtitle {
+  margin-top: 15px;
+}
 ._green {
-  color: green;
+  color: $color-green;
+}
+._red {
+  color: $color-red;
 }
 </style>
